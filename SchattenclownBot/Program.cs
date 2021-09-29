8339e8305bc96430ebfb29b686ca5f4b6b6c3c72
Schattenclown <Tobias.friedl@icloud.com>
2021-09-29 04:46:47 +0200

    oi

--- SchattenclownBot/Program.cs
+++ SchattenclownBot/Program.cs
@@ -23,11 +23,80 @@ namespace SchattenclownBot
         /// <returns>Nothing</returns>
         static async Task Main()
         {
-            DB_ScTimers.CreateTable_ScTimers();
-            DB_ScAlarmClocks.CreateTable_ScAlarmClocks();
+            try
+            {
+                DB_ScTimers.CreateTable_ScTimers();
+                DB_ScAlarmClocks.CreateTable_ScAlarmClocks();
 
-            dBot = new DiscordBot();
-            await dBot.RunAsync();
+                dBot = new DiscordBot();
+                dBot.RunAsync();
+
+                await Task.Delay(1000 * 60 * 60 * 24);
+                Program p = new Program();
+                p.RestartProgram();
+            }
+            catch (Exception)
+            {
+                Program p = new Program();
+                p.RestartProgram();
+            }
+        }
+
+        /// <summary>
+        /// Centers the console.
+        /// </summary>
+        /// <param name="s">The text.</param>
+        static void Center(string s)
+        {
+            try
+            {
+                Console.Write("██");
+                Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
+                Console.Write(s);
+                Console.SetCursorPosition((Console.WindowWidth - 4), Console.CursorTop);
+                Console.WriteLine("██");
+            }
+            catch (Exception)
+            {
+                s = "Console to smoll EXC";
+                Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
+                Console.Write(s);
+                Console.SetCursorPosition((Console.WindowWidth - 4), Console.CursorTop);
+                Console.WriteLine("██");
+            }
+        }
+
+        /// <summary>
+        /// Restarts the program.
+        /// </summary>
+        private async Task RestartProgram()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{"".PadRight(Console.WindowWidth - 2, '█')}");
+            Center(" ");
+            Center(@"██████╗ ███████╗███████╗████████╗ █████╗ ██████╗ ████████╗██╗███╗   ██╗ ██████╗ ");
+            Center(@"██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██║████╗  ██║██╔════╝ ");
+            Center(@"██████╔╝█████╗  ███████╗   ██║   ███████║██████╔╝   ██║   ██║██╔██╗ ██║██║  ███╗");
+            Center(@"██╔══██╗██╔══╝  ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██║██║╚██╗██║██║   ██║");
+            Center(@"██║  ██║███████╗███████║   ██║   ██║  ██║██║  ██║   ██║   ██║██║ ╚████║╚██████╔╝");
+            Center(@"╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝ ");
+            Center(" ");
+            Console.WriteLine($"{"".PadRight(Console.WindowWidth - 2, '█')}");
+            Center("Restating in 10 secounds.");
+            Console.WriteLine($"{"".PadRight(Console.WindowWidth - 2, '█')}");
+            await Task.Delay(1000 * 10);
+
+            dBot.Dispose();
+            // Get file path of current process 
+            var filePath = Assembly.GetExecutingAssembly().Location;
+            //var filePath = Application.ExecutablePath;  // for WinForms
+
+            // Start program
+            Process.Start(filePath);
+
+            // For all Windows application but typically for Console app.
+            Environment.Exit(0);
         }
     }
 }
